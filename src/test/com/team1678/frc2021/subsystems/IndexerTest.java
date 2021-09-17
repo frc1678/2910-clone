74e88e301ae36ccfd463b05880413652d7aebc56
Shuzhengz <treez.zhang@gmail.com>
2021-09-16 20:44:43 -0700

    add test

---
+++ src/test/com/team1678/frc2021/subsystems/IndexerTest.java
@@ -0,0 +1,22 @@
+package com.team1678.frc2021.subsystems;
+
+import junit.framework.TestCase;
+
+public class IndexerTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testGetIndexerVelocity() {
+    }
+
+    public void testSlotsFilled() {
+    }
+
+    public void testSlotsEmpty() {
+    }
+
+    public void testGetInstance() {
+    }
+}
