1a2290298a8fd4bc765901570f5a1c6611e02221
Shuzhengz <treez.zhang@gmail.com>
2021-09-18 19:20:36 -0700

    adds methods and hood

--- src/main/java/com/team1678/frc2021/subsystems/Canifier.java
+++ src/main/java/com/team1678/frc2021/subsystems/Canifier.java
@@ -1,12 +1,19 @@
 package com.team1678.frc2021.subsystems;
 
 import com.ctre.phoenix.CANifier;
+import com.ctre.phoenix.CANifierStatusFrame;
+import com.team1678.frc2021.Constants;
 
 public class Canifier extends Subsystem {
     private static Canifier mInstance;
+    private CANifier mCanifier;
+    private PeriodicInputs mPeriodicInputs;
 
     private Canifier() {
-
+        mCanifier = new CANifier(Constants.kCanifierId);
+        mCanifier.setStatusFramePeriod(CANifierStatusFrame.Status_1_General, 100, Constants.kLongCANTimeoutMs);
+        mCanifier.setStatusFramePeriod(CANifierStatusFrame.Status_2_General, 2, Constants.kLongCANTimeoutMs);
+        mPeriodicInputs = new PeriodicInputs();
     }
 
     public static synchronized Canifier getInstance() {
@@ -16,9 +23,26 @@ public class Canifier extends Subsystem {
         return mInstance;
     }
 
+    public synchronized boolean getHoodLimit() {
+        return mPeriodicInputs.hood_limit_;
+    }
+
+    public int getDeviceId() {
+        return mCanifier.getDeviceID();
+    }
+
     @Override
     public synchronized void readPeriodicInputs() {
         CANifier.PinValues pins = new CANifier.PinValues();
+        mCanifier.getGeneralInputs(pins);
+
+        mPeriodicInputs.indexer_limit_ = !pins.SDA;
+        mPeriodicInputs.hood_limit_ = !pins.SPI_MOSI_PWM1;
+
+    }
+
+    public synchronized CANifier getCanifier() {
+        return mCanifier;
     }
 
     @Override
@@ -37,5 +61,11 @@ public class Canifier extends Subsystem {
 
     @Override
     public void zeroSensors() {
+        mPeriodicInputs = new PeriodicInputs();
+    }
+
+    private static class PeriodicInputs {
+        public boolean indexer_limit_;
+        public boolean hood_limit_;
     }
 }
