146c4e32fcba781c52e6a5668d70edeea0f85b0b
emxia18 <emxia18@gmail.com>
2021-11-11 15:24:00 -0800

    shoot command

---
+++ src/main/java/com/team1678/frc2021/commands/ShootCommand.java
@@ -0,0 +1,26 @@
+package com.team1678.frc2021.commands;
+
+import javax.swing.plaf.metal.MetalInternalFrameTitlePane;
+
+import com.team1678.frc2021.subsystems.Indexer;
+import com.team1678.frc2021.subsystems.Intake;
+import com.team1678.frc2021.subsystems.Superstructure;
+import com.team1678.frc2021.subsystems.Indexer.WantedAction;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class ShootCommand extends CommandBase{
+
+    Superstructure mSuperstructure = Superstructure.getInstance();
+
+    public ShootCommand(Superstructure superstructure) {
+        mSuperstructure = superstructure;
+    }
+
+    @Override
+    public void execute() {
+        mSuperstructure.setWantShoot();
+    }
+
+
+}
