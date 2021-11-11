427190e9864979e20307fb6b9c3080b2f8203b78
emxia18 <emxia18@gmail.com>
2021-11-11 15:33:03 -0800

    add auto vision rotate to target command

---
+++ src/main/java/com/team1678/frc2021/commands/AutoVisionRotateToTargetCommand.java
@@ -0,0 +1,85 @@
+package com.team1678.frc2021.commands;
+
+import com.team1678.frc2021.subsystems.Swerve;
+import com.team1678.frc2021.SwerveTools;
+
+import com.team1678.frc2021.subsystems.Limelight;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import com.team2910.lib.control.PidConstants;
+import com.team2910.lib.control.PidController;
+import com.team2910.lib.math.Vector2;
+
+import java.util.function.DoubleSupplier;
+
+public class AutoVisionRotateToTargetCommand extends CommandBase {
+    private static final PidConstants PID_CONSTANTS = new PidConstants(15.0, 0.0, 1.0);
+
+    private final Swerve drivetrain;
+    private final Limelight limelight;
+
+    private PidController pid_controller = new PidController(PID_CONSTANTS);
+    private double lastTime = 0.0;
+
+    public AutoVisionRotateToTargetCommand(Swerve drivetrain, Limelight limelight) {
+
+        this.drivetrain = drivetrain;
+        this.limelight = limelight;
+
+        addRequirements(drivetrain);
+
+        pid_controller.setInputRange(0.0, 2.0 * Math.PI);
+        pid_controller.setContinuous(true);
+    }
+
+    @Override
+    public void initialize() {
+        lastTime = Timer.getFPGATimestamp();
+        limelight.setCamMode(0);
+        pid_controller.reset();
+    }
+    
+
+    @Override
+    public void execute() {
+        System.out.println("running command");
+        double time = Timer.getFPGATimestamp();
+        double dt = time - lastTime;
+        lastTime = time;
+
+        Translation2d translationalVelocity = new Translation2d(0.0, 0.0);
+
+        double rotationalVelocity = 0.0;
+        if (limelight.seesTarget()) {
+            System.out.println("running inside");
+
+            double currentAngle = drivetrain.getPose().getRotation().getRadians();
+            double targetOffset = Math.toRadians(limelight.getTargetOffset().getAsDouble());
+
+            pid_controller.setSetpoint(currentAngle + targetOffset);
+            rotationalVelocity = pid_controller.calculate(currentAngle, dt);
+            System.out.println(rotationalVelocity);
+
+            /*
+            if (translationalVelocity.length <
+                    Swerve.FEEDFORWARD_CONSTANTS.getStaticConstant() / RobotController.getBatteryVoltage()) {
+                rotationalVelocity += Math.copySign(
+                        0.8 / RobotController.getBatteryVoltage(),
+                        rotationalVelocity
+                );
+            }
+            */
+        }
+        drivetrain.drive(translationalVelocity, rotationalVelocity, true, true);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        // empty
+    }
+}
